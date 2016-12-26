055e119feffc1e4d54021ea32243a67ce536c3cc
Vitaly Svyatyuk <goodgame1945@gmail.com>
2016-12-26 23:09:49 +0300

    a little fix

--- blocked_websites_au.py
+++ blocked_websites_au.py
@@ -1,35 +1,40 @@
 import socket, os, shutil
 
 to_git = False
-with open('new_file.txt.tmp', 'w') as hosts_tmp:
-    with open('new_file.txt', 'r') as hosts:
+with open('hosts/blocked_websites_au.tmp', 'w') as hosts_tmp:
+    with open('hosts/blocked_websites_au', 'r') as hosts:
         for line in hosts:
             line = line.rstrip("\n")
             line_ips = set(line.split(" ")[1:])
             ips = ""
 
-            new_line = socket.gethostbyname_ex(line.split(" ")[0])
+            try:
+                new_line = socket.gethostbyname_ex(line.split(" ")[0])
 
 
-            new_line_ips = set(new_line[2])
+                new_line_ips = set(new_line[2])
 
-            if line_ips == new_line_ips or line_ips.issuperset(new_line_ips):
-                hosts_tmp.write(line + "\n")
-                print("They are the same")
-            else:
-                new_ips = line_ips.union(new_line_ips)
+                if line_ips == new_line_ips or line_ips.issuperset(new_line_ips):
+                    hosts_tmp.write(line + "\n")
+                    print("They are the same")
+                else:
+                    new_ips = line_ips.union(new_line_ips)
 
-                for ip in new_ips:
-                    ips += ip + " "
-                new_line_to_write = new_line[0] + " " + ips.rstrip(" ") + "\n"
+                    for ip in new_ips:
+                        ips += ip + " "
+                    new_line_to_write = new_line[0] + " " + ips.rstrip(" ") + "\n"
 
-                hosts_tmp.write(new_line_to_write)
-                print("Hey! There is a new line!")
-                to_git = True
+                    hosts_tmp.write(new_line_to_write)
+                    print("Hey! There is a new line!")
+                    to_git = True
+            except socket.gaierror:
+                pass
 
-shutil.move('new_file.txt.tmp', 'new_file.txt')
-if to_git:
-    with open('README.md', 'a')
+shutil.move('hosts/blocked_websites_au.tmp', 'hosts/blocked_websites_au')
+
+# if to_git:
+#     with open('README.md', 'a')
+
 # print(new_lines)
 # if len(new_lines) > 0:
 #     with open('new_file.txt', 'w') as new_file:
