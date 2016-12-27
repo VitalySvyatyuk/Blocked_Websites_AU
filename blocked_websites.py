6ca267aed525c12ea42b0e06915422cbb6fe4d05
Vitaly Svyatyuk <goodgame1945@gmail.com>
2016-12-28 01:09:55 +0300

    next

--- blocked_websites.py
+++ blocked_websites.py
@@ -1,31 +1,54 @@
 import socket, os, shutil, datetime
 
+
 to_git = False
-with open('hosts/blocked_websites_au.tmp', 'w') as hosts_tmp:
-    with open('hosts/blocked_websites_au', 'r') as hosts:
-        for line in hosts:
-            line = line.rstrip("\n")
-            line_ip = line.split(" ")[0]
-            print(line_ip)
+updates = {}
+for file in os.listdir("hosts"):
+    PATH_TO_FILE_TMP = 'hosts/' + file + '.tmp'
+    PATH_TO_FILE = 'hosts/' + file
 
-            try:
-                new_line = socket.gethostbyname_ex(line.split(" ")[0])
+    if not os.path.isfile(PATH_TO_FILE_TMP):
+        os.mknod(PATH_TO_FILE_TMP)
 
-                if line_ip in new_line[2]:
-                    hosts_tmp.write(line + "\n")
-                    print("They are the same")
-                else:
-                    new_line_to_write = new_line[2][0] + " " + new_line[0] + "\n"
-                    hosts_tmp.write(new_line_to_write)
-                    print("Hey! There is a new line!")
-                    to_git = True
-            except socket.gaierror:
-                pass
+    with open(PATH_TO_FILE_TMP, 'w') as hosts_tmp:
+        with open(PATH_TO_FILE, 'r') as hosts:
+            update_lines = []
+            for line in hosts:
+                line = line.rstrip("\n")
+                line_ip = line.split(" ")[0]
 
-shutil.move('hosts/blocked_websites_au.tmp', 'hosts/blocked_websites_au')
+                try:
+                    new_line = socket.gethostbyname_ex(line.split(" ")[1])
+                    print(new_line)
+                    print(line_ip)
 
-# if to_git:
-#     with open('README.md', 'a')
+                    if line_ip in new_line[2]:
+                        hosts_tmp.write(line + "\n")
+                        print("They are the same")
+                    else:
+                        new_line_to_write = new_line[2][0] + " " + new_line[0] + "\n"
+                        hosts_tmp.write(new_line_to_write)
+                        print("Hey! There is a new line!->>>>>>>>>>>>")
+                        to_git = True
+                        update_lines.append(new_line[0] + " -> from " + line_ip + " to " + new_line[2])
+                except (socket.gaierror, socket.herror):
+                    pass
+            if len(update_lines) != 0:
+                updates[file] = update_lines
+
+    shutil.move(PATH_TO_FILE_TMP, PATH_TO_FILE)
+    if os.path.isfile(PATH_TO_FILE_TMP):
+        os.remove(PATH_TO_FILE_TMP)
+
+print(updates)
+if to_git:
+    with open('README.md', 'a') as readme:
+        for key, values in updates.items():  
+            readme.write("\n")         
+            readme.write("\n<--- Update for " + key + " from " + str(datetime.datetime.now())[:19] + " --->")
+            for val in values:
+                readme.write("\n" + val)
+            readme.write("\n")
 
 # print(new_lines)
 # if len(new_lines) > 0:
@@ -45,17 +68,18 @@ shutil.move('hosts/blocked_websites_au.tmp', 'hosts/blocked_websites_au')
 
 
 
-import smtplib
-from email.mime.text import MIMEText
+# --- EMAIL SENDING FOR CHECKING SCRIPT WORKING ---
+# import smtplib
+# from email.mime.text import MIMEText
 
-computername = os.getenv('COMPUTERNAME')
-msg = MIMEText('Hello from {}'.format(datetime.datetime.now()))
-msg['Subject'] = 'Test from {}!'.format(computername)
-email_from = '{}@test.com'.format(computername)
-email_to = 'vetal_sv@bk.ru'
-msg['From'] = email_from
-msg['To'] = email_to
+# computername = os.getenv('COMPUTERNAME')
+# msg = MIMEText('Hello from {}'.format(datetime.datetime.now()))
+# msg['Subject'] = 'Test from {}!'.format(computername)
+# email_from = '{}@test.com'.format(computername)
+# email_to = 'vetal_sv@bk.ru'
+# msg['From'] = email_from
+# msg['To'] = email_to
 
-s = smtplib.SMTP('127.0.0.1')
-s.sendmail(email_from, [email_to], msg.as_string())
-s.quit()
+# s = smtplib.SMTP('127.0.0.1')
+# s.sendmail(email_from, [email_to], msg.as_string())
+# s.quit()
